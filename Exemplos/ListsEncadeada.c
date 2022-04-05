1a4d1be1a22b6d9ee854d8b6da15131d1c669578
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-04-05 10:09:08 -0300

    Exemplos e exercícios aprendendo a usar o CLion e o RunCodes

---
+++ Exemplos/ListsEncadeada.c
@@ -0,0 +1,68 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//struct lista {
+//    int info;
+//    struct lista *prox;
+//};
+//typedef struct lista Tlista;
+//
+//Tlista *cria_lista (void) {
+//    return NULL;
+//}
+//
+//Tlista *insere_inicio (Tlista *li, int i){
+//    Tlista *novo = (Tlista*) malloc (sizeof (Tlista));
+//    novo -> info = i;
+//    novo -> prox = li;
+//    return novo;
+//}
+//
+//Tlista *insere_fim (Tlista *li, int i){
+//    Tlista *novo = (Tlista*) malloc (sizeof(Tlista));
+//    novo -> info = i;
+//    novo -> prox = NULL;
+//    Tlista *p = li;
+//    Tlista *ant = li;
+//    while (p != NULL){ /* encontra o ultimo elemento*/
+//        ant = p; /* ant vai sempre apontar pro elemento anterior */
+//        p = p -> prox;
+//    }
+//    if (ant != NULL) /* se a lista original não estiver vazia */
+//        ant -> prox = novo;
+//    else
+//        li = novo;
+//    return li;
+//}
+//
+//void imprime_lista (Tlista *li){
+//    Tlista *p;
+//    for (p = li; p != NULL; p = p ->prox)
+//        printf(" %d", p ->info);
+//}
+//
+//int listaSimples (void) {
+//    Tlista* L = NULL;
+//    int entrada, tamanhoLista, escolha;
+//
+//    printf("\nDigite a quantidade de elementos para inserir:");
+//    scanf("%d", &tamanhoLista);
+//
+//    for (int i = 0; i < tamanhoLista;  i++){
+//        printf("\nPara inserir no inicio (1), Para inserir no fim (0):");
+//        scanf("%d", &escolha);
+//
+//        printf("\nDigite o numero a ser inserido:");
+//        scanf("%d", &entrada);
+//
+//        if (escolha)
+//            L = insere_inicio(L, entrada);
+//        else
+//            L = insere_fim(L, entrada);
+//    }
+//
+//    printf("info: ");
+//    imprime_lista(L);
+//
+//    return 0;
+//}
