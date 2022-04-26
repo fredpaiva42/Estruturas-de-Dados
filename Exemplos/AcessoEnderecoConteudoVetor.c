23ef36b6549ecf1b4e87d6f82d2f96c842838ba5
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-04-26 09:51:49 -0300

    Exercícios Avaliativos e de treino no Run Codes.

--- Exemplos/AcessoEnderecoConteudoVetor.c
+++ Exemplos/AcessoEnderecoConteudoVetor.c
@@ -1,30 +1,30 @@
-/* Cálculo da média de 10 números reais */
-#include <stdio.h>
-
-float media (int n, float *v){
-    int i;
-    float s = 0.0;
-    for (i = 0; i < n; i++)
-        s += v[i];
-    return s/n;
-}
-
-int main (void) {
-    float v[10];
-    float med = 0.0;
-    int i;
-
-    /* leitura dos valores */
-    for (i = 0; i < 10; i++)
-        scanf("%f", &v[i]); // acesso ao endereço da i-ésima posição de v
-
-//    /* cálculo da média */
+///* Cálculo da média de 10 números reais */
+//#include <stdio.h>
+//
+//float media (int n, float *v){
+//    int i;
+//    float s = 0.0;
+//    for (i = 0; i < n; i++)
+//        s += v[i];
+//    return s/n;
+//}
+//
+//int main (void) {
+//    float v[10];
+//    float med = 0.0;
+//    int i;
+//
+//    /* leitura dos valores */
 //    for (i = 0; i < 10; i++)
-//        med = med + v[i]; // acesso ao conteúdo de v[i]
-//    med = med / 10;
-    med = media(10, v);
-
-    /* exibição do resultado */
-    printf("Media = %f \n", med);
-    return 0;
-}
+//        scanf("%f", &v[i]); // acesso ao endereço da i-ésima posição de v
+//
+////    /* cálculo da média */
+////    for (i = 0; i < 10; i++)
+////        med = med + v[i]; // acesso ao conteúdo de v[i]
+////    med = med / 10;
+//    med = media(10, v);
+//
+//    /* exibição do resultado */
+//    printf("Media = %f \n", med);
+//    return 0;
+//}
